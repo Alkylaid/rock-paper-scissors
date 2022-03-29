08efd8b2d38e155362b5ed308f38853c637d9fd9
Alkylaid <alkylaid@gmail.com>
2022-03-28 23:16:46 -0500

    Create computer selection function

---
+++ javascript.js
@@ -0,0 +1,17 @@
+function computerPlay() {
+    randomNumber = Math.floor(Math.random() * 3);
+
+    switch (randomNumber) {
+        case 0: 
+            return "Rock";
+            break;
+        case 1:
+            return "Paper";
+            break;
+        case 2:
+            return "Scissors";
+            break;
+    }
+
+}
+
