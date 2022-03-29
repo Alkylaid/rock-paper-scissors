6120636d0a2b4fb99d5d03fdb8fbb5250310f4a0
Alkylaid <alkylaid@gmail.com>
2022-03-28 23:17:19 -0500

    Create function to play round with computer

--- javascript.js
+++ javascript.js
@@ -15,3 +15,24 @@ function computerPlay() {
 
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "Draw!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore++;
+        return `You win! ${playerSelection} beats ${computerSelection}`
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore++;
+        return `You win! ${playerSelection} beats ${computerSelection}`
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++;
+        return `You win! ${playerSelection} beats ${computerSelection}`
+    } else {
+        computerScore++;
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    }
+    
+
+}
