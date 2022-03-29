46cd5e5ed042028bc71c722487748b11fdea0132
Alkylaid <alkylaid@gmail.com>
2022-03-28 23:34:33 -0500

    Create game function that plays game 5 rounds

--- javascript.js
+++ javascript.js
@@ -16,23 +16,49 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    
 
     if (playerSelection === computerSelection) {
-        return "Draw!";
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return `Draw! Player: ${playerScore} Computer: ${computerScore}`;
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
         playerScore++;
         return `You win! ${playerSelection} beats ${computerSelection}`
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
         playerScore++;
-        return `You win! ${playerSelection} beats ${computerSelection}`
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return `You win! ${playerSelection} beats ${computerSelection}. Player: ${playerScore} Computer: ${computerScore}`
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
         playerScore++;
-        return `You win! ${playerSelection} beats ${computerSelection}`
+        return `You win! ${playerSelection} beats ${computerSelection}. Player: ${playerScore} Computer: ${computerScore}`
     } else {
         computerScore++;
-        return `You lose! ${computerSelection} beats ${playerSelection}`
+        return `You lose! ${computerSelection} beats ${playerSelection}. Player: ${playerScore} Computer: ${computerScore}`
     }
     
 
 }
+
+function game() {
+    
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = window.prompt("Rock, Paper, Scissors?");
+        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+
+        
+        const computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    if (playerScore === computerScore) {
+        return "Draw!";
+    } else if (playerScore > computerScore) {
+        return "Player wins!";
+    } else {
+        return "Computer Wins!";
+    }
+}
+
+
+let playerScore = 0;
+let computerScore = 0;
+
+
+console.log(game());
